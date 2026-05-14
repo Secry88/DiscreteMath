f0160f55356254ac18f4a27d6f2fdb3499395dc0
eighty-eighth <artemka4837@gmail.com>
2026-05-14 03:44:25 +0300

    updates

--- Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+401fc6a655cad3eb2c3777824403246ede251ddb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b6a3c97045841e8094111b418a34196582048d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
